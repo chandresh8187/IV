03d5626c92e3139632090718f99420031c40495a
chandresh8187 <borichachandresh35@gmail.com>
2026-05-26 18:56:49 +0530

    host done

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://app.ivsquarestructure.com/api',
   timeout: 15000,
   headers: {
     'Content-Type': 'application/json',
