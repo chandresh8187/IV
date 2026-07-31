e71833cb8f2136dfbae4e8fce756368550b0b560
chandresh8187 <borichachandresh35@gmail.com>
2026-07-31 14:56:01 +0530

    statusbar changes

--- index.js
+++ index.js
@@ -5,8 +5,10 @@
 import { registerRootComponent } from 'expo';
 import messaging from '@react-native-firebase/messaging';
 import App from './App';
+import { StatusBar } from 'react-native';
 
 // Keep this outside all React components
+StatusBar.setBarStyle('light-content');
 messaging().setBackgroundMessageHandler(async remoteMessage => {
   console.log('Background notification:', remoteMessage);
 });
